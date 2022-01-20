d06f5886861e423d32595a6dfe4afad070a303a7
ezeun <consistency1231@gmail.com>
2022-01-20 18:11:41 +0900

    메인 네비게이션 탭바 만들기

--- app/src/main/java/com/example/todaysoutput/MainActivity.kt
+++ app/src/main/java/com/example/todaysoutput/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.todaysoutput
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.example.todaysoutput.navigation.*
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        when(p0.itemId){
+            R.id.action_home ->{
+                var detaionViewFragment = DetailViewFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,detaionViewFragment).commit()
+                return true
+            }
+            R.id.action_search ->{
+                var gridFragment = GridFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,gridFragment).commit()
+                return true
+            }
+            R.id.action_add_photo ->{
+
+                return true
+            }
+            R.id.action_favorite_alarm ->{
+                var alarmFragment = AlarmFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,alarmFragment).commit()
+                return true
+            }
+            R.id.action_account ->{
+                var userFragment = UserFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,userFragment).commit()
+                return true
+            }
+        }
+        return false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
     }
+
 }
